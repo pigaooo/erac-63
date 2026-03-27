3f8effca6862a8b598b1f402a8217944696b8d00
Thiago Sarkis <pigaooo@gmail.com>
2026-03-26 22:02:07 -0300

    Atualiza seed de lojas com dados da programacao

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,17 +28,41 @@ class DatabaseSeeder extends Seeder
 
         $userIds = User::query()->pluck('id');
 
-        Loja::insert(collect([
-            ['id' => Str::ulid(), 'name' => 'Loja Acacia do Oriente', 'numero_loja' => 101, 'email' => 'contato@acaciaoriente.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Luz e Trabalho', 'numero_loja' => 102, 'email' => 'contato@luzetrabalho.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Fraternidade e Virtude', 'numero_loja' => 103, 'email' => 'contato@fraternidadevirtude.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Harmonia Universal', 'numero_loja' => 104, 'email' => 'contato@harmoniauniversal.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Colunas do Saber', 'numero_loja' => 105, 'email' => 'contato@colunasdosaber.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Templo da Verdade', 'numero_loja' => 106, 'email' => 'contato@templodaverdade.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Estrela do Sul', 'numero_loja' => 107, 'email' => 'contato@estreladosul.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Guardioes do Segredo', 'numero_loja' => 108, 'email' => 'contato@guardioesdosegredo.test'],
-            ['id' => Str::ulid(), 'name' => 'Loja Filhos da Luz', 'numero_loja' => 109, 'email' => 'contato@filhosdaluz.test'],
-        ])->map(function (array $loja) use ($userIds) {
+        $nomesLojas = [
+            'Acacia de Ubatuba',
+            'Amor a Ordem Respeitada',
+            'Fraternidade Academica Sementes do Amanha',
+            'Estrela Vega',
+            'Vigilantes de Taubate',
+            'Fraternidade Academica Irmao Jose Geraldo Trani Brandao',
+            'Fraternidade Academica Luciano Alfredo Vianna do Rio',
+            'Arquitetos da Harmonia',
+            'Independencia e Lealdade',
+            'Integridade e Justica',
+            'Luz do Oriente',
+            'Luz, Vida e Amor',
+            'Solidariedade do Paraitinga',
+            'Templarios da Paz',
+            'Uniao das Americas',
+            'Uniao, Forca e Vigor',
+            'Universitaria Cavaleiros do Sol',
+            'Fonte de Vida',
+            'Vinte e Um de Abril',
+            'Natureza e Fraternidade',
+            'Fraternidade e Integridade Taubateana',
+            'Colunas de Luz',
+        ];
+
+        Loja::insert(collect($nomesLojas)->values()->map(function (string $nome, int $index) use ($userIds) {
+            $slug = Str::slug($nome, '');
+
+            $loja = [
+                'id' => Str::ulid(),
+                'name' => $nome,
+                'numero_loja' => 101 + $index,
+                'email' => "contato@{$slug}.test",
+            ];
+
             $loja['user_id'] = $userIds->random();
 
             return $loja;
