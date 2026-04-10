75208ec7f6e19c3d1985fd267846faf95be155fb
Thiago Sarkis <pigaooo@gmail.com>
2026-04-09 22:26:00 -0300

    Add UserResource with CRUD pages and tests for user management

---
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditUser extends EditRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (blank($data['password'] ?? null)) {
+            unset($data['password']);
+        }
+
+        return $data;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
+    }
+}
